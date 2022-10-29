54fc7351c37ff7eef955ef8192450bc30579aa7b
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-29 13:09:20 +0530

    created object and called analyseMood method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string result = moodAnalyser.analyseMood("I am in Sad Mood");
+            Console.WriteLine(result);
+        }
+    }
+}
