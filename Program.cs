6f77e96fcf65632d5c5c9040febacb44b281053e
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-29 13:19:39 +0530

    cleared the main method of driver class

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string result = moodAnalyser.analyseMood("I am in Sad Mood");
-            Console.WriteLine(result);
+            
+
+
+
+
+
+
         }
     }
 }
